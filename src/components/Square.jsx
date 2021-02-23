6f5b6ed1eaccb9953448d4442ee6d786c1fac2e7
Will <wdportman@gmail.com>
2021-02-23 09:54:40 -0800

    Add styling to squares

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 export default function Square(props) {
 
+  const style = {
+    background: 'lightblue',
+    border: '2px solid darkblue',
+    fontSize: '30px',
+    fontWeight: '800',
+    cursor: 'pointer',
+    outline: 'none'
+  };
+
   const {onClick, value} = props;
 
   return (
-    <button onClick = {onClick}>
+    <button style={style} onClick = {onClick}>
     {value}
   </button>
   )
